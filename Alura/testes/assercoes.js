ccbe97992692a649aaec73294d271718fa243066
jamescosjr <jamescosjr@gmail.com>
2025-12-02 15:46:09 -0300

    feat: adicionar arquivos de configuração e funções básicas

---
+++ Alura/testes/assercoes.js
@@ -0,0 +1,7 @@
+if (2 === 2) //true
+
+if (15 > 42) //false
+
+if (2 >= 2) //true
+
+['banana', 'abacaxi', 'uva'].includes('banana') //true
---
+++ Alura/testes/index.js
@@ -0,0 +1,3 @@
+const somaHoraExtras = (salario, valorHorasExtras) => salario + valorHorasExtras;
+
+console.log(somaHoraExtras(2000, 500));
